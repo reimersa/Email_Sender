3aacf47e7c9e9cf10ee33a92d7691e531d411b36
Vinicius Massami Mikuni <vinicius.massami.mikuni@cern.ch>
2020-11-30 10:05:59 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Sending automatic emails with attachments with python
+
+This repo is initially intended for TAs at UZH that are required to send corrected homeworks to a list of students.
+
+# Instructions
+
+Download the repo with:
+
+```bash
+git clone https://github.com/ViniciusMikuni/Email_Sender.git
+```
+The script requires the following structure:
+
+* A file called ```students.txt```, containing the name, surname and email of each student (Exactly like the excell sheet we first received with the student list). Example:
+
+```bash
+John Doe john.doe@uzh.ch
+Joe Doe joe.doe@uzh.ch
+```
+
+A folder called ```HWN```, where ```N``` stands for the homework number which files are saved. Ex: ```HW7```. Save the homeworks in this folder after correction. The name of the files in the folder shpuld follow the same pattern the students are required to follow.
+
+# Running the script:
+
+```bash
+python send_emails.py --saner your.email@uzh.ch --pwd YOURPASSWORD --HW N
+```
+
+Before sending, verify in the screen if all students are associated to the respective homeworks and emails. If everything seems right, run the same code again, but with the flag ```--send```:
+
+```bash
+python send_emails.py --saner your.email@uzh.ch --pwd YOURPASSWORD --HW N --send
+```
+
---
+++ send_emails.py
@@ -0,0 +1,100 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+from email.mime.application import MIMEApplication
+from optparse import OptionParser
+import os
+
+
+
+def MIMESETUP(flags,receiver,subject,base_email,attachment):
+    '''Mime config'''
+
+    #Setup the MIME
+    message = MIMEMultipart()
+    message['From'] = flags.sender
+    message['To'] = receiver
+    message['Subject'] = subject
+    #The subject line
+    #The body and the attachments for the mail
+    message.attach(MIMEText(base_email, 'plain'))
+    
+    with open(attachment, 'rb') as f: # Open the file as binary mode
+        attach = MIMEApplication(f.read(),_subtype="pdf")
+        
+    attach.add_header('Content-Decomposition', 'attachment', filename=attachment)
+    message.attach(attach)
+    return message
+
+def CreateSession(flags,message):    
+    #Create SMTP session for sending the mail
+    session = smtplib.SMTP('smtp.uzh.ch', 587) #use gmail with port
+    session.starttls() #enable security
+    session.login(flags.sender, flags.pwd) #login with mail_id and password
+    text = message.as_string()
+    session.sendmail(flags.sender, message['To'], text)
+    session.quit()
+    print('Mail Sent to {}'.format(message['To']))
+
+def GetStudentInfo(HW,names='students.txt'):
+    ''' Find the name, email and respective homework attachment for each student.
+    Returns a dictionary containing the student's name, email and corresponding pdf file
+    '''
+    students={}
+    with open(names,'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            line = line.strip('\n')
+            name = line.split('\t')[1]
+            surname = line.split('\t')[0]
+
+            def GetFile(name,surname,HW):
+                homeworks = os.listdir("HW{}".format(HW))
+                if name.split(" ")[-1] != name:
+                    name = name.split(" ")[-1]
+                if surname.split(" ")[-1] != surname:
+                    surname = surname.split(" ")[-1]
+                for homework in homeworks:
+                    if name.lower() in homework.lower() and surname.lower() in homework.lower():
+                        return os.path.join("HW{}".format(HW),homework)
+                    
+                print("Student {} didn't hand the homework".format(name))
+                return None
+            
+            if GetFile(name,surname,HW) != None:
+                students["{} {}".format(name,surname)] = {'email':line.split('\t')[-1],'file':GetFile(name,surname,HW)}
+    return students
+
+    
+
+def SendEmails(flags):
+    ''' Send emails with attachments '''
+    names  = GetStudentInfo(flags.HW)
+
+    subject = 'HW{} results'.format(flags.HW)
+    for name in names:
+        base_email = '''Hello {},
+In attachment is your corrected homework. Let me know in case you have questions or if the corrections do not display properly.
+Cheers,
+Vinicius
+        '''.format(name)
+        #print(base_email)
+
+        print("Student: {} \nFile: {}: \nEmail: {} \n".format(name,names[name]['file'],names[name]['email']))
+        if flags.send:
+            msg = MIMESETUP(flags,names[name]['email'],subject,base_email,names[name]['file'])
+            CreateSession(flags,msg)
+
+    
+
+
+if __name__=='__main__':
+    parser = OptionParser(usage="%prog [opt]  inputFiles")
+    parser.add_option("--HW", type=int, default=7, help="Specify the homework number")
+    parser.add_option("--sender", type="string", default="your_email@uzh.ch", help="Specify the sender's email")
+    parser.add_option("--pwd", type="string", default="", help="Specify the sender's password")
+    parser.add_option("--send", action="store_true", default=False, help="Send the emails.")
+    (flags, args) = parser.parse_args()
+    SendEmails(flags)
