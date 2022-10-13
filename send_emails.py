2ed2efe9ea17a961b91cb2f2ad5bf6ff5fbe1a79
Arne Reimers <arne.reimers@cern.ch>
2022-10-13 11:19:10 +0200

    Updated script

--- send_emails.py
+++ send_emails.py
@@ -23,82 +23,153 @@ def MIMESETUP(flags,receiver,subject,base_email,attachment):
     message = MIMEMultipart()
     message['From'] = flags.sender
     message['To'] = receiver
+    message['Cc'] = flags.sender
     message['Subject'] = subject
     #The subject line
     #The body and the attachments for the mail
     message.attach(MIMEText(base_email, 'plain'))
-    
+
     with open(attachment, 'rb') as f: # Open the file as binary mode
         attach = MIMEApplication(f.read(),_subtype="pdf")
-        
+
     attach.add_header('Content-Decomposition', 'attachment', filename=attachment)
     message.attach(attach)
     return message
 
-def CreateSession(flags,message):    
+def CreateSession(flags,message):
     #Create SMTP session for sending the mail
-    session = smtplib.SMTP('smtp.uzh.ch', 587) #use gmail with port
+    session = smtplib.SMTP('smtp.cern.ch', 587) #use gmail with port
     session.starttls() #enable security
     session.login(flags.sender, flags.pwd) #login with mail_id and password
     text = message.as_string()
-    session.sendmail(flags.sender, message['To'], text)
+    session.sendmail(flags.sender, [message['To'], message['Cc']], text)
     session.quit()
-    print('Mail Sent to {}\n\n'.format(message['To']))
+    print('Mail sent to {}\n'.format(message['To']))
 
-def GetStudentInfo(HW,names='students.txt'):
-    ''' Find the name, email and respective homework attachment for each student.
-    Returns a dictionary containing the student's name, email and corresponding pdf file
-    '''
-    students={}
-    with codecs.open(names,'r', encoding="utf-8") as f:
+
+# take one line from students.csv. For each, check if a file with this XXX.YYY exists (without @zzz.AA, because some students have ethz addresses)
+# if such a corresponding file exists, return the entire filename and the complete mailaddress
+def get_hw_and_address_from_line(line, hwfolder, filename_must_contain_any_of=None):
+    address_full = line.split(',')[3].lower()
+    address_firstpart = address_full.split('@')[0]
+
+    # look for address_firstpart in content of folder
+    files_in_folder = os.listdir(hwfolder)
+    correctfile = None
+    for f in files_in_folder:
+        if address_firstpart in f.lower():
+            if filename_must_contain_any_of is not None:
+                for part in filename_must_contain_any_of:
+                    if part in f:
+                        if correctfile is None:
+                            correctfile = f
+                        else:
+                            raise ValueError('For address there seem to be multiple matching files, \'%s\' and \'%s\'' % (correctfile, f))
+            else:
+                if correctfile is None:
+                    correctfile = f
+                else:
+                    raise ValueError('For address there seem to be multiple matching files, \'%s\' and \'%s\'' % (correctfile, f))
+                break
+
+    if correctfile is None:
+        print 'Did not find a file for address %s' % (address_full)
+        return (None, None)
+    else:
+        print 'Nice, found this file for address %s: %s' % (address_full, correctfile)
+        return (address_full, os.path.join(hwfolder, correctfile))
+
+
+
+
+def GetStudentInfo(HW,studentlist='students.csv'):
+
+    # takes number of HW and filename of studentlist in csv format
+    # returns tuple of: (list of tuples with addresses and corresponding files, list of all files [for bookkeeping])
+
+    addresses_and_filenames = []
+    hwfolder = 'Sheet%02i' % (HW)
+    with open(studentlist, 'r') as f:
         lines = f.readlines()
         for line in lines:
-            line = line.strip('\n')
-            name = line.split('\t')[1]
-            surname = line.split('\t')[0]
+            addresses_and_filenames.append(get_hw_and_address_from_line(line=line, hwfolder=hwfolder, filename_must_contain_any_of=['Sheet%02i' % (HW), 'sheet%02i' % (HW)]))
 
-            def GetFile(name,surname,HW):
-                homeworks = [unicodedata.normalize('NFC', f) for f in os.listdir(u"HW{}".format(HW))]
-                if name.split(" ")[-1] != name:
-                    name = name.split(" ")[-1]
-                if surname.split(" ")[-1] != surname:
-                    surname = surname.split(" ")[-1]
-                for homework in homeworks:
-                    if name.lower().decode('UTF-8') in homework.lower() and surname.lower().decode('UTF-8') in homework.lower(): #TODO: implement accent support 
+    return addresses_and_filenames, [os.path.join(hwfolder, f) for f in os.listdir(hwfolder)]
 
-                        return os.path.join("HW{}".format(HW),homework)
-                    
-                print("Student '{} {}' didn't hand the homework".format(name, surname))
-                return None
-            
-            if GetFile(name,surname,HW) != None:
-                students["{} {}".format(name,surname)] = {'email':line.split('\t')[-1],'file':GetFile(name,surname,HW)}
-    return students
 
-    
+    # students={}
+    # with codecs.open(names,'r', encoding="utf-8") as f:
+    #     lines = f.readlines()
+    #     for line in lines:
+            # line = line.strip('\n')
+            # name = line.split(',')[1]
+            # surname = line.split(',')[2]
+
+
+
+            # def GetFile(name,surname,HW):
+            #     homeworks = os.listdir("Sheet%02i"%(HW))
+            #     if name.split(" ")[-1] != name:
+            #         name = name.split(" ")[-1]
+            #     if surname.split(" ")[-1] != surname:
+            #         surname = surname.split(" ")[-1]
+            #     for homework in homeworks:
+            #         if name.lower().decode('UTF-8') in homework.lower() and surname.lower().decode('UTF-8') in homework.lower(): #TODO: implement accent support
+            #
+            #             print("-- Nice, student '{} {}' handed in the homework".format(name, surname))
+            #             return os.path.join("sheet%02i"%(HW),homework)
+
+                # print("Student '{} {}' didn't hand the homework".format(name, surname))
+                # return None
+
+            # if GetFile(name,surname,HW) != None:
+            #     students["{} {}".format(name,surname)] = {'email':line.split(',')[3],'file':GetFile(name,surname,HW)}
+    # return students
+
+
 
 def SendEmails(flags):
     ''' Send emails with attachments '''
-    names  = GetStudentInfo(flags.HW)
+    (addresses_and_filenames, allfiles)  = GetStudentInfo(flags.HW)
 
-    subject = 'HW{} results'.format(flags.HW)
-    for name in names:
-        base_email = '''Hello {},
-In attachment is your corrected homework. Let me know in case you have questions or if the corrections do not display properly.
+    subject = 'PHY211 sheet {} results'.format(flags.HW)
+    addresses_used = []
+    files_used = []
+    for (address, filename) in addresses_and_filenames:
+    # for name in names:
+        base_email = '''Hi,
+
+Attached you can find your marked solutions for sheet {}.
+
 Cheers,
-Vinicius
-        '''.format(name)
+Kyle & Arne
+        '''.format(flags.HW)
         #print(base_email)
 
-        print("Student: {} \nFile: {}: \nEmail: {} \n".format(name,names[name]['file'],names[name]['email']))
-        if flags.send:
-            msg = MIMESETUP(flags,names[name]['email'],subject,base_email,names[name]['file'])
-            CreateSession(flags,msg)
+        # print("File: {}: \nEmail: {} \n".format(names[name]['file'],names[name]['email']))
+        # print names[name]['email']
 
-def RemoveSpecialCharacters(string): 
+        if filename is not None:
+            if address in addresses_used:
+                raise ValueError('Trying to send to address %s, but it already appeared before... duplicate entry in CSV?')
+            addresses_used.append(address)
+            files_used.append(filename)
+            print 'Sending: %s --> %s' % (filename, address)
+            if flags.send:
+
+                msg = MIMESETUP(flags=flags,receiver=address,subject=subject,base_email=base_email,attachment=filename)
+                CreateSession(flags,msg)
+    print 'Sent %i mails' % (len(addresses_used))
+    files_not_used = [f for f in allfiles if f not in files_used]
+    if len(files_not_used) > 0:
+        print '\n\n--> WARNING: Not all files in the folder have been sent (%i leftover), some maybe named badly? Here they are:' % (len(files_not_used))
+    for f in files_not_used:
+        print ' ', f
+
+def RemoveSpecialCharacters(string):
     return "".join(e for e in string if e.isalnum())
 
-    
+
 
 
 if __name__=='__main__':
